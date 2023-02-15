c7b15e35a039afb46855e6c59b32d1c9e95bdc47
Steveb Mendez <Stevenampaiz@gmail.com>
2023-02-14 23:31:05 -0600

    Repository Pattern & Dependency Injection

---
+++ PokemonReviewsApp/Controllers/PokemonController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewsApp.Interfaces;
+using PokemonReviewsApp.Models;
+
+namespace PokemonReviewsApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+        private readonly IPokemonRepository _pokemonRepository;
+
+        public PokemonController(IPokemonRepository pokemonRepository)
+        {
+            _pokemonRepository = pokemonRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+        public IActionResult GetPokemons()
+        {
+            var pokemons = _pokemonRepository.GetPokemons();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(pokemons);
+        }
+    }
+}
---
+++ PokemonReviewsApp/Repository/PokemonRepository.cs
@@ -0,0 +1,21 @@
+﻿using PokemonReviewsApp.Data;
+using PokemonReviewsApp.Interfaces;
+using PokemonReviewsApp.Models;
+
+namespace PokemonReviewsApp.Repository
+{
+    public class PokemonRepository : IPokemonRepository
+    {
+        public readonly DataContext _context;
+
+        public PokemonRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection<Pokemon> GetPokemons()
+        {
+            return _context.Pokemons.OrderBy(p => p.Id).ToList();
+        }
+    }
+}
