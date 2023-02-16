e06049b6f26fc0832d760357111f3ed444c9e6ed
Steveb Mendez <Stevenampaiz@gmail.com>
2023-02-15 23:36:22 -0600

    GET & Read Methods [PART 1]

---
+++ PokemonReviewsApp/Dto/PokemonDto.cs
@@ -0,0 +1,11 @@
+﻿using PokemonReviewsApp.Models;
+
+namespace PokemonReviewsApp.Dto
+{
+    public class PokemonDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public DateTime BirthDate { get; set; }
+    }
+}
--- PokemonReviewsApp/Repository/PokemonRepository.cs
+++ PokemonReviewsApp/Repository/PokemonRepository.cs
@@ -13,9 +13,34 @@ namespace PokemonReviewsApp.Repository
             _context = context;
         }
 
+        public Pokemon GetPokemon(int id)
+        {
+            return _context.Pokemons.Where(pokemon => pokemon.Id == id).FirstOrDefault()!;
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+            return _context.Pokemons.Where(pokemon => pokemon.Name == name).FirstOrDefault()!;
+        }
+
+        public decimal GetPokemonRating(int pokeId)
+        {
+            var review = _context.Reviews.Where(review => review.Pokemon.Id == pokeId);
+
+            if (review is null || !review.Any())
+                return 0;
+
+            return Convert.ToDecimal(review.Average(review => review.Rating));
+        }
+
         public ICollection<Pokemon> GetPokemons()
         {
-            return _context.Pokemons.OrderBy(p => p.Id).ToList();
+            return _context.Pokemons.OrderBy(pokemon => pokemon.Id).ToList();
+        }
+
+        public bool PokemonExits(int pokemonId)
+        {
+            return _context.Pokemons.Any(pokemon => pokemon.Id == pokemonId);
         }
     }
 }
