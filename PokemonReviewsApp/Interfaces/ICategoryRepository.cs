f377c96f5335489b4de8200caa2f5073659027de
Steveb Mendez <Stevenampaiz@gmail.com>
2023-02-17 23:36:06 -0600

    ICategoryRepository

---
+++ PokemonReviewsApp/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using PokemonReviewsApp.Models;
+
+namespace PokemonReviewsApp.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        ICollection<Category> GetCategories();
+        Category GetCategory();
+        ICollection<Pokemon> GetPokemonByCategory(int categoryId);
+        bool CategoryExists(int id);
+    }
+}
